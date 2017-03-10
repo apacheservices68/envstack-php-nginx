0b8106c42505f5ff48658d3af88c7bf0f883dd55
Bành Vinh <vinh.banh@applancer.net>
2017-03-10 15:07:29 +0700

    A

---
+++ README.md
@@ -0,0 +1,9 @@
+Docker container : 
+- Ubuntu 16.04 
+- nginx latest
+- php7 
+- mongo 
+- redis latest
+- elasticsearch 2.3
+- rabbitmq 3 management
+- mariadb latest
---
+++ app/code/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vinh Banh <apacheservices68@gmail.com>
+ * Date: 06/03/2017 - Time: 13:56
+ * Version: 1.0
+ */
+#phpinfo();
+echo "123";
